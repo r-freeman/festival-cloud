a135735db08d6ceb24d7975384c7ae8e0062cca9
Ryan Freeman <N00173767@student.iadt.ie>
2021-04-08 18:11:33 +0100

    Set up RDS

--- classes/Connection.php
+++ classes/Connection.php
@@ -1,12 +1,24 @@
 <?php
 
+if (isset($_SERVER['ENVIRONMENT']) && $_SERVER['ENVIRONMENT'] == 'production') {
+    define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
+    define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+    define('DB_USER', $_SERVER['RDS_USERNAME']);
+    define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
+} else {
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'festival_cloud');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+}
+
 class Connection
 {
 
-    private static $host = "localhost";
-    private static $database = "festival_cloud";
-    private static $username = "root";
-    private static $password = "";
+    private static $host = DB_HOST;
+    private static $database = DB_NAME;
+    private static $username = DB_USER;
+    private static $password = DB_PASSWORD;
 
     public static function getInstance()
     {
